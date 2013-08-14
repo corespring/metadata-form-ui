827dcc0151516be1de2668d6af5f26aba691e4d5
edeustace <ed.eustace@gmail.com>
2013-08-14 21:47:30 +0200

    add all back

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -4,6 +4,9 @@
 //= require "bower_components/underscore/underscore"
 //= require "bower_components/angular-ui/build/angular-ui"
 //= require "bower_components/corespring-ng-components/build/corespring-ng-components"
+//= require "metadata-form-ui"
+//= require "controllers/_declaration"
+//= require_tree "controllers"
 
 
 
